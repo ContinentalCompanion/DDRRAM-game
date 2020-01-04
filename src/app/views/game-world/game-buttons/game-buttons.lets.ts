2bdf539bdfdcc2efca958a909fa93bae79618acb
ContinentalCompanion <continentalcompanion@gmail.com>
2020-01-03 23:03:30 -0800

    Fixed game button tooltips.

--- src/app/views/game-world/game-buttons/game-buttons.lets.ts
+++ src/app/views/game-world/game-buttons/game-buttons.lets.ts
@@ -14,28 +14,28 @@ export const gameButtons = [
         id: 'memeButton',
         name: 'Attempt Meme',
         icon: gameActionIcons.memeIcon,
-        description: 'Spend the day attempting to meme. Success chance can be improved by Train Memery.',
+        description: 'Spend the day<br>attempting to meme.<br>Success chance can be<br>improved by Train Memery.',
         action: 'meme'
     },
     {
         id: 'trainMemeryButton',
         name: 'Train Memery',
         icon: gameActionIcons.trainMemeryIcon,
-        description: 'Spend the day improving memery skills. Increases Attempt Meme success chance by 2%.',
+        description: 'Spend the day<br>improving memery skills.<br>Increases Attempt Meme<br>success chance by 2%.',
         action: 'trainMemery'
     },
     {
         id: 'workButton',
         name: 'Work',
         icon: gameActionIcons.workIcon,
-        description: 'Go to work for the day. Earns money equal to your salary.',
+        description: 'Go to work for the day.<br>Earns money equal<br>to your salary.',
         action: 'work'
     },
     {
         id: 'educationButton',
         name: 'Attempt Education',
         icon: gameActionIcons.educationIcon,
-        description: 'Spend the day attempting to improve your career. 25% chance to increase salary by $2.',
+        description: 'Spend the day attempting<br>to improve your career.<br>25% chance to increase<br>salary by $2.',
         action: 'education'
     }
 ];
